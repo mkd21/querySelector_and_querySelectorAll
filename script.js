11ad2a4bf873d96c246fab24090f4f5c3b419a60
Mayank Deep <mayankdeep217@gmail.com>
2024-02-04 22:23:56 +0530

    added files of querySelector

---
+++ script.js
@@ -0,0 +1,15 @@
+        // by directly selecting the tag also 
+// let x = document.querySelector("h1");
+
+        // by using id 
+// let x = document.querySelector("#firstHeading");
+
+        // by chaning also 
+
+// let x = document.querySelector("#parent h1");
+
+
+// aur fir .style laga ke available property sue krlo 
+
+// ul wala reference leke dekh lo usecase kya hai 
+
